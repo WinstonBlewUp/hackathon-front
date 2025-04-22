ec7f321031873cbfb199615eea89fa10f8589bf6
cecilelecerf <c.lecerf.cecile@gmail.com>
2025-04-22 22:28:33 +0200

    init

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,17 @@
+
+import { Card, Text } from "@mantine/core";
+
+export default function Home() {
+  return (
+    <div  >
+      <main >
+        <Card shadow="md" sx={{ background: "var(--mantine-color-red-5)" }}>
+          <Text>
+            Init
+          </Text>
+        </Card>
+      </main>
+
+    </div>
+  );
+}
